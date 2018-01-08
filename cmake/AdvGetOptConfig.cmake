27bbcacef787b3dfe447ccde1f61d606aa355017
AlexisWilke <alexis@m2osw.com>
2018-01-07 17:41:49 -0800

    Removed the suffix for the advgetopt include folder.
    Added support for an additional directory one can specify using the -D command line option (i.e. ADVGETOPT_LIBRARY_DIR)
    Fixed the documentation and reference the correct paths and not the intermediate ones as those one is expected to use.

--- cmake/AdvGetOptConfig.cmake
+++ cmake/AdvGetOptConfig.cmake
@@ -3,23 +3,23 @@
 # Once done this will define
 #
 # ADVGETOPT_FOUND        - System has AdvGetOpt
-# ADVGETOPT_INCLUDE_DIR  - The AdvGetOpt include directories
-# ADVGETOPT_LIBRARY      - The libraries needed to use AdvGetOpt (none)
+# ADVGETOPT_INCLUDE_DIRS - The AdvGetOpt include directories
+# ADVGETOPT_LIBRARIES    - The libraries needed to use AdvGetOpt
 # ADVGETOPT_DEFINITIONS  - Compiler switches required for using AdvGetOpt (none)
 
 find_path( ADVGETOPT_INCLUDE_DIR advgetopt/advgetopt.h
-			PATHS $ENV{ADVGETOPT_INCLUDE_DIR}
-			PATH_SUFFIXES advgetopt
+			PATHS ENV ADVGETOPT_INCLUDE_DIR
 		 )
 find_library( ADVGETOPT_LIBRARY advgetopt
-			PATHS $ENV{ADVGETOPT_LIBRARY}
+			PATHS ${ADVGETOPT_LIBRARY_DIR} ENV ADVGETOPT_LIBRARY
 		 )
 mark_as_advanced( ADVGETOPT_INCLUDE_DIR ADVGETOPT_LIBRARY )
 
-set( ADVGETOPT_INCLUDE_DIRS ${ADVGETOPT_INCLUDE_DIR} ${ADVGETOPT_INCLUDE_DIR}/advgetopt )
+set( ADVGETOPT_INCLUDE_DIRS ${ADVGETOPT_INCLUDE_DIR} )
 set( ADVGETOPT_LIBRARIES    ${ADVGETOPT_LIBRARY}     )
 
 include( FindPackageHandleStandardArgs )
+
 # handle the QUIETLY and REQUIRED arguments and set ADVGETOPT_FOUND to TRUE
 # if all listed variables are TRUE
 find_package_handle_standard_args( AdvGetOpt DEFAULT_MSG ADVGETOPT_INCLUDE_DIR ADVGETOPT_LIBRARY )
