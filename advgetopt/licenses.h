0092aea6722cd274421522427d8879c042f6a73a
AlexisWilke <alexis@m2osw.com>
2019-07-20 14:57:57 -0700

    Added the actual license files.

---
+++ advgetopt/licenses.h
@@ -0,0 +1,47 @@
+/*
+ * File:
+ *    advgetopt/licenses.h -- a set of licenses you can readily use in your tool
+ *
+ * License:
+ *    Copyright (c) 2006-2019  Made to Order Software Corp.  All Rights Reserved
+ *
+ *    https://snapwebsites.org/
+ *    contact@m2osw.com
+ *
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License along
+ *    with this program; if not, write to the Free Software Foundation, Inc.,
+ *    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Authors:
+ *    Alexis Wilke   alexis@m2osw.com
+ *    Doug Barbieri  doug@m2osw.com
+ */
+#pragma once
+
+/** \file
+ * \brief Definitions of the advanced getopt class.
+ *
+ * The advgetopt library offers an advanced way to manage your command
+ * line tools options on the command line, in environment variables, and
+ * in configuration files.
+ */
+
+namespace advgetopt
+{
+
+
+extern char const * 		g_license_gpl_v2;
+
+
+}   // namespace advgetopt
+// vim: ts=4 sw=4 et
